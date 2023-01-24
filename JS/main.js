44f1f6b7ab9ca73a48e1aa2926e3da5644ff9cd0
netwests <minecrafterhorser@gmail.com>
2023-01-24 10:20:01 +0700

    New name!

--- JS/main.js
+++ JS/main.js
@@ -2,9 +2,9 @@ const textContainer = document.getElementById("ctext");
             window.onscroll = function() {
             var scroll = window.scrollY;
             if (scroll > 20) {
-                textContainer.innerHTML = "NW";
+                textContainer.innerHTML = "VF";
             }
             if (scroll < 20) {
-                textContainer.innerHTML = "NETWESTS";
+                textContainer.innerHTML = "VAPORFROM";
             }
         }
