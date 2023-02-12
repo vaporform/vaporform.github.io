a75f0139c17ac5a6e985ed918072b70e483f9713
vaporfrom <minecrafterhorser@gmail.com>
2023-02-13 02:59:57 +0700

    Fixed typewriter,

--- JS/main.js
+++ JS/main.js
@@ -11,7 +11,7 @@ const textContainer = document.getElementById("ctext");
 
 
 const textContainer2 = document.getElementById("rtext");
-const textList = ["Vapor's Playground", "Hello visitor!","Buu EDU", "Is Pi a pie?", "Github Pages","NEWBORNED WEBSITE"];
+const textList = ["Vapor's Playground", "Hello visitor!","Buu EDU", "Is Pi a pie?", "Github Pages","NEWBORNED WEBSITE","I make music... Sometimes.","Gotta add more projetcs :T"];
         
 function displayRandomText() {
   var randomIndex = Math.floor(Math.random() * textList.length);
