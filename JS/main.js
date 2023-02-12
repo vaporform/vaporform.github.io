e183c54b6b604384869501b4a03a00a8ca42faf5
vaporfrom <minecrafterhorser@gmail.com>
2023-02-13 01:35:46 +0700

    improve

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,21 @@
 const textContainer = document.getElementById("ctext");
-            window.onscroll = function() {
+        window.onscroll = function() {
             var scroll = window.scrollY;
             if (scroll > 20) {
                 textContainer.innerHTML = "VF";
-            }
+                }
             if (scroll < 20) {
                 textContainer.innerHTML = "VAPORFROM";
-            }
+                }
         }
+
+
+const textContainer2 = document.getElementById("rtext");
+const textList = ["Vapor's Playground", "Hello visitor!","Buu EDU", "Is Pi a pie?", "Github Pages","NEWBORNED WEBSITE"];
+        
+function displayRandomText() {
+  var randomIndex = Math.floor(Math.random() * textList.length);
+  textContainer2.innerHTML ="<b>"+ textList[randomIndex]+ "</b>";    }
+
+  displayRandomText();
+        
